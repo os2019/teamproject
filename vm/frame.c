2e7d086208f3b9f4330b8cf388a031f46986c11d
cleart <clear1014@gmail.com>
2019-06-03 17:14:31 +0900

    타입 수정 및 frame table length 삭제

--- vm/frame.c
+++ vm/frame.c
@@ -15,31 +15,35 @@ struct frame_table_entry {
 static struct lock ft_lock;
 static struct {
     struct frame_table_entry * entry;
-    uint8_t length;
+    size_t length;
 } frame_table;
 
-void frame_init(uint8_t frame_cnt){
+void frame_init(size_t frame_cnt){
     lock_init(&ft_lock);
-    frame_table.length = frame_cnt;
+    //frame_table.length = frame_cnt;
     frame_table.entry = palloc_get_page(false);
 }
 
 void add_frame(struct thread* t, void* upage, void* kpage){
-    //printf("%s %p %d\n", t->name, kpage, get_user_index(kpage));
-    lock_acquire(&ft_lock);
-    frame_table.entry[get_user_index(kpage)].upage = upage;
-    frame_table.entry[get_user_index(kpage)].t = t;
-    lock_release(&ft_lock);
+    add_frame_multiple(t,upage,kpage,1);
 }
 
+void add_frame_multiple(struct thread* t, void* upage, void* kpage, size_t page_cnt){
+    lock_acquire(&ft_lock);
+    for(size_t i = 0; i < page_cnt; i++){
+        printf("%s %p %p %d\n", t->name, upage, kpage, get_user_index(kpage) + i);
+        frame_table.entry[get_user_index(kpage) + i].upage = upage;
+        frame_table.entry[get_user_index(kpage) + i].t = t;
+    }
+    lock_release(&ft_lock);
+}
 void remove_frame(void * kpage){
     remove_frame_multiple(kpage,1);
 }
 
-void remove_frame_multiple(void * kpage, uint8_t page_cnt){
+void remove_frame_multiple(void * kpage, size_t page_cnt){
     lock_acquire(&ft_lock);
-    for(int i = 0; i < page_cnt; i++){
-        //printf("%s %p\n",frame_table.entry[get_user_index(kpage)+i].t->name,frame_table.entry[get_user_index(kpage)+i]);
+    for(size_t i = 0; i < page_cnt; i++){
         frame_table.entry[get_user_index(kpage)+i].upage = NULL;
         frame_table.entry[get_user_index(kpage)+i].t = NULL;
     }
--- vm/frame.h
+++ vm/frame.h
@@ -5,12 +5,12 @@
 #include <stdint.h>
 
 
-void frame_init(uint8_t frame_cnt);
+void frame_init(size_t frame_cnt);
 
 void add_frame(struct thread* t, void* upage, void* kpage);
-
+void add_frame_multiple(struct thread* t, void* upage, void* kpage, size_t page_cnt);
 void remove_frame(void * kpage);
-void remove_frame_multiple(void * kpage, uint8_t page_cnt);
+void remove_frame_multiple(void * kpage, size_t page_cnt);
 
 
 #endif
